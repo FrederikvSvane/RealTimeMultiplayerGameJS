99b48366489b4d0cf8c218e79646a91ed8445097
Lucas Juel <lucas.juel.sjostrom@gmail.com>
2024-02-21 17:06:33 +0100

    followed video

--- index.ts
+++ index.ts
@@ -19,19 +19,68 @@ const wsServer = new websocketServer({
 
 
 const clients = {};
-
+const games = {};
 
 wsServer.on("request", request => {
     const connection = request.accept(null, request.origin);
+
+    const clientId = uuidv4();
+    clients[clientId] = {
+        "connection": connection
+    };
+
     connection.on("open", () => console.log("opened!"));
-    connection.on("close", () => console.log("closed!"));
+    connection.on("close", () => console.log("Connection is closed!"));
     connection.on("message", message => {
         const result = JSON.parse(message.utf8Data); //This fails if the message is not a JSON
         console.log("Received Message:", result);
+
+        //Create new game
+        if(result.method === "create"){
+            const clientId = result.clientId;
+            const gameId = uuidv4();
+            games[gameId] = {
+                "id": gameId,
+                "balls": 20,
+                "clients": []
+            }
+
+            const payload = {
+                "method": "create",
+                "game": games[gameId]
+            }
+            const con = clients[clientId].connection;
+            console.log(games[gameId]);
+            con.send(JSON.stringify(payload));
+        } 
+        
+        else if(result.method === "join"){
+            const clientId = result.clientId;
+            const gameId = result.gameId;
+            const game = games[gameId];
+            if (game.clients.length > 3){
+                //Max 3 players
+                return;
+            }
+
+            const color = {"0": "Red", "1": "Green", "2": "Blue"}[game.clients.length];
+            game.clients.push({
+                "clientId": clientId,
+                "color": color
+            })
+
+            const payLoad = {
+                "method": "join",
+                "game": games[gameId],
+            }
+
+            game.clients.forEach(c => {
+                const con = clients[c.clientId].connection;
+                con.send(JSON.stringify(payLoad));
+            })   
+        }
     })
 
-    const clientId = uuidv4();
-    clients[clientId] = connection;
     const payload = {
         "method": "connect",
         "clientId": clientId
